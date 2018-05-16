d93e9978d60d785f795b910d27fe57bf66e25dd8
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-05-16 15:07:48 -0500

    basic map

---
+++ src/components/App/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      starList: ['Menkar', 'Kochab', 'Hadar'],
+    };
+  }
+
+  render() {
+    // let starListItemArray = [];
+    // for loop here
+    // for (let i = 0; i < this.state.starList.length; i++) {
+    //   let starName = this.state.starList[i]
+    //   starListItemArray.push(<li key={starName}>{starName}</li>);
+    // }
+
+    const starListItemArray = this.state.starList.map(starName => {
+      const newStar = <li key={starName}>{starName}</li>;
+      return newStar;
+    });
+
+
+    return (
+      <div className="App">
+        <p>Here's the list of stars</p>
+        <p>The first item in the array is: {this.state.starList[0]}</p>
+        <ul>
+          {starListItemArray}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
